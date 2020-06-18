93dbdd025c705d0ceb6bdb4fcbccf288c0243d2a
ksawant1@binghamton.edu <ksawant1@binghamton.edu>
2020-06-18 15:38:39 -0400

    created four action methods in the state interface for adding, removing videos, calculating metrics and for advertisment requests

--- channelpopularity/src/channelpopularity/state/StateI.java
+++ channelpopularity/src/channelpopularity/state/StateI.java
@@ -1,5 +1,9 @@
 package channelpopularity.state;
 
 public interface StateI {
+    void addvideo();
+    void removevideo();
+    void calmetrics(int likes,int dislikes,int views);
+    void adrequest(int length);
 
 }
