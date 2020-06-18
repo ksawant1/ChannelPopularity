916ee8b015585e5beb85b39de6ba082a5aed2fa7
ksawant1@binghamton.edu <ksawant1@binghamton.edu>
2020-06-18 15:37:04 -0400

    created an instance of context class and added a video

--- channelpopularity/src/channelpopularity/context/ContextI.java
+++ channelpopularity/src/channelpopularity/context/ContextI.java
@@ -1,5 +1,8 @@
 package channelpopularity.context;
 
 public interface ContextI {
+        
+
+
 
 }
--- channelpopularity/src/channelpopularity/driver/Driver.java
+++ channelpopularity/src/channelpopularity/driver/Driver.java
@@ -1,7 +1,7 @@
 package channelpopularity.driver;
-
+import channelpopularity.context.Context2;
 /**
- * @author John Doe
+ * @author Krupa Sawant
  *
  */
 public class Driver {
@@ -13,11 +13,14 @@ public class Driver {
 		 * As the build.xml specifies the arguments as input,output or metrics, in case the
 		 * argument value is not given java takes the default value specified in
 		 * build.xml. To avoid that, below condition is used
-		 */
+		*/
 		if ((args.length != 2) || (args[0].equals("${input}")) || (args[1].equals("${output}"))) {
 			System.err.printf("Error: Incorrect number of arguments. Program accepts %d arguments.", REQUIRED_NUMBER_OF_CMDLINE_ARGS);
 			System.exit(0);
 		}
 		System.out.println("Hello World! Lets get started with the assignment");
+		
+		Context2 context = new Context2();
+		context.addvideo();
 	}
 }
