1efea0a1d52457b1759a5a66607392f3d3068aa7
ksawant1 <54869724+ksawant1@users.noreply.github.com>
2020-06-21 19:26:35 +0530

    implements the state factory interface and overrides the create method which instantiates enums representing particular states

--- channelpopularity/src/channelpopularity/state/factory/SimpleStateFactory.java
+++ channelpopularity/src/channelpopularity/state/factory/SimpleStateFactory.java
@@ -1,10 +1,27 @@
 package channelpopularity.state.factory;
 
+import channelpopularity.state.HighlyPopularState;
+import channelpopularity.state.MildlyPopularState;
+import channelpopularity.state.StateI;
+import channelpopularity.state.StateName;
+import channelpopularity.state.UltraPopularState;
+import channelpopularity.state.UnpopularState;
+
 public class SimpleStateFactory implements SimpleStateFactoryI {
-    StateI create(Enum event){
-        StateI state=null;
-        if(event.equals(UNPOPULAR.event)){
-            state=new UNPOPULAR();
+
+    @Override
+    public StateI create(Enum event) {
+        switch (StateName.enumValueOf(event.name())) {
+            case ULTRA_POPULAR:
+                return new UltraPopularState();
+            case HIGHLY_POPULAR:
+                return new HighlyPopularState();
+            case MILDLY_POPULAR:
+                return new MildlyPopularState();
+            case UNPOPULAR:
+                return new UnpopularState();
+            default:
+                return null;
         }
     }
 }
