0160f86c4a991b18f68e1899501ed945c00bfc3e
ksawant1 <54869724+ksawant1@users.noreply.github.com>
2020-06-24 05:22:42 +0530

    separated common methods in abstract state class

--- channelpopularity/src/channelpopularity/state/AbstractState.java
+++ channelpopularity/src/channelpopularity/state/AbstractState.java
@@ -1,5 +1,32 @@
 package channelpopularity.state;
 
+import static channelpopularity.state.StateName.ULTRA_POPULAR;
+import static channelpopularity.state.StateName.HIGHLY_POPULAR;
+import static channelpopularity.state.StateName.MILDLY_POPULAR;
+import static channelpopularity.state.StateName.UNPOPULAR;
+
 public abstract class AbstractState implements StateI {
 
+	@Override
+	public StateName getCurrentState(double popularity) {
+
+		if (popularity > 100000)
+			return ULTRA_POPULAR;
+		if (popularity > 10000)
+			return HIGHLY_POPULAR;
+		if (popularity > 1000)
+			return MILDLY_POPULAR;
+		else
+			return UNPOPULAR;
+	}
+
+	@Override
+	public int getMinLength() {
+		return 1;
+	}
+
+	@Override
+	public boolean adRequest(int length) {
+		return getMinLength() < length && length <= getMaxLength();
+	}
 }
